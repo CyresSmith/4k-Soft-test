5d5d791054b7f06730ae2d28c02c206626acc4aa
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-08-18 12:17:30 +0300

    Updates

---
+++ assets/Container-4a444224.js
@@ -0,0 +1,9 @@
+import{n as i,t,j as a}from"./index-3e67b549.js";const o=i.div`
+  width: ${t.mediaBreakpoints.desktop.width};
+  height: 100%;
+  padding: 0 ${t.space[3]};
+  margin: 0 auto;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`,r=({children:e,id:n=null})=>a.jsx(o,{id:n,children:e});export{r as C};
---
+++ assets/FormBox.styled-5c25b23c.js
@@ -0,0 +1,8 @@
+import{n as o}from"./index-3e67b549.js";import{F as r}from"./notiflix-aio-3.2.6.min-c526fbc5.js";const i=o(r)`
+  width: 500px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 100px;
+  gap: 60px;
+`;export{i as F};
