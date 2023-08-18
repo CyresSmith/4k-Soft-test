2e4aad6e624cb90e2fe07a742fceffb47f9d7bdd
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-08-18 19:55:11 +0300

    change basename

--- src/main.jsx
+++ src/main.jsx
@@ -13,7 +13,8 @@ createRoot(document.getElementById('root')).render(
   <StrictMode>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
-        <BrowserRouter basename="4k-soft-test">
+        {/* <BrowserRouter basename="4k-soft-test"> */}
+        <BrowserRouter>
           <App />
         </BrowserRouter>
       </PersistGate>
