10719ea12e17fb1e25197b16d4765ebfc06117cd
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2023-08-20 20:09:04 +0300

    add/dist

---
+++ dist/assets/Title-85f14986.js
@@ -0,0 +1,15 @@
+import{n as i,t as e,j as s}from"./index-95628fc1.js";const r=i.section`
+  margin: ${e.space[4]} 0;
+
+  @media (${e.mediaBreakpoints.mobile.media}) {
+    overflow-y: auto;
+  }
+  @media ${e.mediaBreakpoints.tablet.media} {
+    overflow-y: auto;
+  }
+`,l=i.p`
+  font-size: ${t=>e.fontSizes[t.size]};
+  font-weight: ${e.fontWeights.bold};
+  line-height: ${e.lineHeights.l};
+  text-align: ${t=>t.align};
+`,x=({as:t="h1",size:o="xxxl",align:n="center",children:a})=>s.jsx(l,{as:t,size:o,align:n,children:a});export{r as S,x as T};
